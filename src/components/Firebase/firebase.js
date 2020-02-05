b0725fb52b1850a626376ad2c51bb06c3b7c68be
unknown <g00359656@gmit.ie>
2020-02-05 20:50:15 +0000

    Initial Commit

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyD0_iLSieVAyXYLShSEYKUVUjX-PKl-oQ4",
+  authDomain: "gym-app-2310d.firebaseapp.com",
+  databaseURL: "https://gym-app-2310d.firebaseio.com",
+  projectId: "gym-app-2310d",
+  storageBucket: "gym-app-2310d.appspot.com",
+  messagingSenderId: "377524844099",
+}
+
+class Firebase {
+    constructor(){
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+    }
+
+    // Auth API
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+    
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => 
+        this.auth.currentUser.updatePassword(password);
+    
+}
+
+export default Firebase;
